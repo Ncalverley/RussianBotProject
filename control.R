4319a6cf39d7951edee7fcff618f610aafc3210b
Ncalverley2 <Ncalverley2@gmail.com>
2018-07-12 13:49:07 -0600

    First commit of items into the repo.

---
+++ control.R
@@ -0,0 +1,27 @@
+####################################################################################
+##                          RUSSIAN BOT PROJECT
+##
+## This program will use Natural Language Processing to analyze a data set of tweets
+## published by confirmed Russian-linked accounts that were used to influence public
+## option prior to the 2016 presidential election. The data set contains tweets from
+## 454 accounts that have been confirmed by Twitter as Russian troll accounts.
+####################################################################################
+
+## Load all libraries
+source(file = "R/loadPackages.R")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
