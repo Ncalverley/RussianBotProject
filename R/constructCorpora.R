52a9d02df8074a5f80c07ba1553c8e5085c78a5d
Ncalverley2 <Ncalverley2@gmail.com>
2018-07-13 09:23:50 -0600

    Putting in a bunch of cleaning functions to clean the tweets.

---
+++ R/constructCorpora.R
@@ -0,0 +1,60 @@
+####################################################################################
+##                            CONSTRUCT CORPORA
+##
+## This file will import the twitter data containing the full tweets of all the 
+## Russian-linked troll accounts, and construct the corpora for each account.
+####################################################################################
+
+
+####################################################################################
+## Load the data
+####################################################################################
+
+tweetData <- import_loadTweets()
+
+## Take a small sample for development
+tweetData <- tweetData[1:100, ]
+
+####################################################################################
+## Clean the tweets by removing contaminants
+####################################################################################
+
+## Remove hashtags
+tweetData$text <- apply(tweetData[match("text", names(tweetData))], 1, scrub_removeString, strType = "hashtag")
+
+## Remove URLs
+tweetData$text <- apply(tweetData[match("text", names(tweetData))], 1, scrub_removeString, strType = "url")
+
+## Remove User IDs
+tweetData$text <- apply(tweetData[match("text", names(tweetData))], 1, scrub_removeString, strType = "user")
+
+## Remove Retweets
+tweetData$text <- apply(tweetData[match("text", names(tweetData))], 1, scrub_removeString, strType = "retweet")
+
+## Remove garbage characters
+tweetData$text <- apply(tweetData[match("text", names(tweetData))], 1, scrub_removeString, strType = "garbage")
+
+## Re-run the functions to remove hashtags, URLs, etc. The removal of the garbage
+## characters will have revealed some additional contaminants.
+
+## Remove hashtags
+tweetData$text <- apply(tweetData[match("text", names(tweetData))], 1, scrub_removeString, strType = "hashtag")
+
+## Remove URLs
+tweetData$text <- apply(tweetData[match("text", names(tweetData))], 1, scrub_removeString, strType = "url")
+
+## Remove User IDs
+tweetData$text <- apply(tweetData[match("text", names(tweetData))], 1, scrub_removeString, strType = "user")
+
+## Remove Retweets
+tweetData$text <- apply(tweetData[match("text", names(tweetData))], 1, scrub_removeString, strType = "retweet")
+
+####################################################################################
+## Run a spellchecker on the tweets - we want to identify any mis-spelled words and
+## replace them with their correct versions.
+####################################################################################
+
+
+
+
+
--- R/loadPackages.R
+++ R/loadPackages.R
@@ -13,12 +13,176 @@
 
 library(data.table)
 library(ggplot2)
-
+library(hunspell)
 
 
 ####################################################################################
-## Initialize custom functions
+## Initialize data import functions
 ####################################################################################
+
+#' Import Tweet Data
+#' 
+#' Import the CSV containing the tweets of the Russian troll accounts.
+#'
+#' @return A data frame containing the raw tweet data.
+import_loadTweets <- function() {
+  data <- read.csv(file = "data/tweets.csv", stringsAsFactors = FALSE)
+  return(data)
+}
+
+
+####################################################################################
+## Initialize lexical manipulation functions
+####################################################################################
+
+#' Remove Garbage
+#' 
+#' This is a utility function that will remove garbage characters from a word. 
+#' Garbage is defined as non-alpha-numeric characters, or technical computer
+#' language like the linebreak command. This function will remove all garbage
+#' from a word, and return the cleaned word.
+#' 
+#' @param x A string.
+#'
+#' @return A logical indicating whether or not the string is garbage.
+utils_removeGarbage <- function(x = NA) {
+  ## Define the list of characters that are considered garbage.
+  garbage <- c("\n", "&amp")
+  ## Remove all garbage characters from the word
+  for(i in garbage) x <- gsub(i, "", x)
+  ## Remove all punctuation except # and @. We need to preserve
+  ## these symbols so that other functions can detect and remove
+  ## entire hashtags and user IDs from tweets.
+  x <- gsub("(?!#)(?!@)[[:punct:]]", "", x, perl=TRUE)
+  ## Remove all non-ASCII characters
+  x <- iconv(x, "latin1", "ASCII", sub="")
+  ## Return the result
+  return(x)
+}
+
+#' Remove String
+#' 
+#' Tweets often contain strings that are undesirable when constructing a corpus -
+#' things like URLs and hashtags. This is a wrapper function that will remove any
+#' such undesirable string from a tweet.
+#' 
+#' @param txt A vector containing a list of words.
+#' @param strType The type of string to be removed. Options include 'hashtag', 'url',
+#' 'user', and 'retweet'.
+#'
+#' @return The same vector of words but with any and all hashtags removed.
+scrub_removeString <- function(txt = NA, strType = NA) {
+  ## Break out the data into individual words
+  txt <- unlist(strsplit(txt, split = " "))
+  ## Identify any 
+  switch(strType, 
+         "garbage" = {
+           txt <- sapply(txt, utils_removeGarbage)
+           return(paste(txt, sep = " ", collapse = " "))
+         },
+         "hashtag" = {
+           target <- sapply(txt, utils_identHashtag)
+           return(paste(txt[!target], sep = " ", collapse = " "))
+         },
+         "url" = {
+           target <- sapply(txt, utils_identURL)
+           return(paste(txt[!target], sep = " ", collapse = " "))
+         },
+         "user" = {
+           target <- sapply(txt, utils_identUser)
+           return(paste(txt[!target], sep = " ", collapse = " "))
+         },
+         "retweet" = {
+           target <- sapply(txt, utils_identRetweet)
+           return(paste(txt[!target], sep = " ", collapse = " "))
+         }
+  )
+}
+
+#' Identify Hashtag
+#' 
+#' This is a utility function that will identify whether or not a word is
+#' a hashtag. Any word that begins with the pound sign will result in this
+#' function returning a value of TRUE. All other words will return FALSE.
+#' 
+#' @param x A string.
+#'
+#' @return A logical indicating whether or not the string is a hashtag.
+utils_identHashtag <- function(x = NA) {
+  ## Break the word into its constituent letters
+  x <- unlist(strsplit(x, split = ""))
+  ## If the first letter is a hashtag, return true. Otherwise, return false.
+  if(length(x) > 0) {
+    if(x[1] == "#") {
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
+  } else {
+    return(FALSE)
+  }
+}
+
+#' Identify URL
+#' 
+#' This is a utility function that will identify whether or not a word is
+#' a URL. Any word that contains two forward slashes will result in this
+#' function returning a value of TRUE. All other words will return FALSE.
+#' 
+#' @param x A string.
+#'
+#' @return A logical indicating whether or not the string is a URL.
+utils_identURL <- function(x = NA) {
+  ## If the string contains a web address, return true. Otherwise, return false.
+  if(length(grep("://", x)) > 0){
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
+#' Identify User
+#' 
+#' This is a utility function that will identify whether or not a word is
+#' a user ID. Any word that begins with the @ sign will result in this
+#' function returning a value of TRUE. All other words will return FALSE.
+#' 
+#' @param x A string.
+#'
+#' @return A logical indicating whether or not the string is a user ID.
+utils_identUser <- function(x = NA) {
+  ## Break the word into its constituent letters
+  x <- unlist(strsplit(x, split = ""))
+  ## If the first letter is a hashtag, return true. Otherwise, return false.
+  if(length(x) > 0) {
+    if(x[1] == "@") {
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
+  } else {
+    return(FALSE)
+  }
+}
+
+#' Identify Retweet
+#' 
+#' This is a utility function that will identify whether or not a word is
+#' a retweet. Any word whose entire value is "RT" will result in this
+#' function returning a value of TRUE. All other words will return FALSE.
+#' 
+#' @param x A string.
+#'
+#' @return A logical indicating whether or not the string is a retweet.
+utils_identRetweet <- function(x = NA) {
+  ## If the string contains a web address, return true. Otherwise, return false.
+  if(x == "RT"){
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
 
 
 
